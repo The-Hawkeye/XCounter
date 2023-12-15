ee2af4189c8134f49a87685850fcfd89a2c76c7f
The-Hawkeye <82801422+The-Hawkeye@users.noreply.github.com>
2023-12-15 18:25:08 +0530

    gjhgjh

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ function App() {
 
 
   return (
-    <div>
+    <>
     <h1>Counter App</h1>
-    <h3>Count : {counter}</h3>
+    <h3>Count:{counter}</h3>
 
     <button onClick={handleIncrease}>
         Increment
     </button>
 
     <button onClick={handleDecrease}>Decrement</button>
-</div>
+    </>
   );
 }
 
